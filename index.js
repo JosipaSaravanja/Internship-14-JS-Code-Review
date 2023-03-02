06596f1e5c6e93c214d8fd44b1a961cdab1989e6
Josipa Šaravanja <josipa.saravanja06@gmail.com>
2023-03-03 00:26:53 +0100

    added basic files, how comments and code should look (posible some css changes)

---
+++ index.js
@@ -0,0 +1,7 @@
+function closeOverlay(){
+    document.getElementsByClassName("overlay")[0].style.display = "none";
+}
+
+function openOverlay(){
+    document.getElementsByClassName("overlay")[0].style.display = "block";
+}
